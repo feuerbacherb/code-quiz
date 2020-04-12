1d414e5a473064e478ee3c534a09566ac5ed125c
Brent F <feuerbacherb@gmail.com>
2020-04-12 12:39:01 -0500

    finished scoring section

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,11 @@ var theAnswer = "";
 var numRight = 0;
 var numWrong = 0;
 var finalScore = document.getElementById("final-score");
+var percentCorrect = 0;
+var initials = document.getElementById("save-init");
+var highScoreDisplay = document.getElementById("high-score-display");
+var btnSave = document.getElementById("save");
+var oldScore = "";
 
 
 var setTime = function() {
@@ -28,8 +33,16 @@ var setTime = function() {
          alert("You ran out of time!");
          var total = numRight + numWrong;
 
+         percentCorrect = Math.round((numRight/total) * 100)
+
          timer.textContent = "Timer: 0";
-         finalScore.innerHTML = "Your answered " + numRight + " questions correctly<br>out of the " + total + " questions attempted."
+         finalScore.innerHTML = "<h2>You scored " + percentCorrect + "%!</h2>Your answered " + numRight + " questions correctly<br>out of the " + total + " questions attempted."
+         if (parseInt(oldScore) > percentCorrect) {
+            btnSave.style.display = "none";
+         }
+         else {
+            btnSave.style.display = "";
+         }
          showScore();
       } 
       else {
@@ -74,7 +87,23 @@ var btnHandler = function(event) {
             getQuestions();
          }
       }
-      
+   }
+   else if (targetE1.matches(".save")) {
+      console.log("save fired");
+      saveScore();
+   }
+   else if (targetE1.matches(".confirm")) {
+      var origInit = document.getElementById("init").elements["initials"].value;
+      localStorage.setItem("initials", origInit);
+      localStorage.setItem("score", percentCorrect)
+      location.reload();
+   }
+   else if (targetE1.matches(".try-again")) {
+      location.reload();
+   }
+   else if (targetE1.matches(".cancel")) {
+      alert("Thank you for playing!");
+      location.reload();
    }
 }
 
@@ -85,10 +114,24 @@ var showQuestions = function() {
    docHighScores.style.display = "none";
 }
 
+var saveScore = function() {
+   docIntro.style.display = "none";
+   docQuestions.style.display = "none";
+   docFinishQuiz.style.display = "none";
+   docHighScores.style.display = "block";
+}
+
 var showScore = function() {
    docIntro.style.display = "none";
    docQuestions.style.display = "none";
    docFinishQuiz.style.display = "block";
+   docHighScores.style.display = "none";
+}
+
+var showIntro = function() {
+   docIntro.style.display = "block";
+   docQuestions.style.display = "none";
+   docFinishQuiz.style.display = "none";
    docHighScores.style.display = "none";
 }
 
@@ -121,6 +164,17 @@ var getQuestions = function() {
    arrQuestions.splice(objID,1);
    console.log(arrQuestions.length);
    arrQuestions.length;
+}
+
+var chkLS = function() {
+   var inits = localStorage.getItem("initials");
+   oldScore = localStorage.getItem("score");
+   if (inits === null || oldScore === null) {
+      highScoreDisplay.style.display = "none";
+   } else {
+      highScoreDisplay.style.display = "block";
+      highScoreDisplay.innerHTML = "Highest Score: " + inits.toUpperCase() + ": " + oldScore + "%";
+   }
 }
 
 pageContentE1.addEventListener("click", btnHandler);
